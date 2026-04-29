3efb33dcae1ff70dde7183c452cf1fa91fac510d
Kênia Guimaraes <keniiarruda@gmaill.com>
2026-04-29 20:50:51 -0300

    feat: implement organic tech design system and plant dashboard

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,27 +1,39 @@
 class PlantsController < ApplicationController
   def index
     @plants = Plant.all
-    render json: @plants
+    respond_to do |format|
+      format.html # Renderiza a view HTML
+      format.json { render json: @plants }
+    end
   end
 
   def show
     @plant = Plant.find(params[:id])
-    render json: @plant.as_json(include: :care_parameters)
+    @care_parameters = @plant.care_parameters
+    respond_to do |format|
+      format.html # Renderiza a view HTML
+      format.json { render json: @plant, include: :care_parameters }
+    end
   end
 
   def create
     @plant = Plant.new(plant_params)
-
     if @plant.save
-      render json: @plant, status: :created
+      respond_to do |format|
+        format.html { redirect_to plants_path, notice: "Planta criada com sucesso!" }
+        format.json { render json: @plant, status: :created }
+      end
     else
-      render json: @plant.errors, status: :unprocessable_entity
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @plant.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   private
 
   def plant_params
-    params.require(:plant).permit(:name, :image)
+    params.require(:plant).permit(:name, :species, :nickname, :image)
   end
 end
