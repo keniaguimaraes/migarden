575cc30462ebbe56aaa2f5a879dd971d72837ea7
Kênia Guimaraes <keniiarruda@gmaill.com>
2026-04-30 18:32:53 -0300

    fix: disable encrypted credentials and remove .enc file

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Migarden
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Disable encrypted credentials
+    config.credentials.enabled = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
