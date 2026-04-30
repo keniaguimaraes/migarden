b160715190eca2d363c6ad390ef29908308ea187
Kênia Guimaraes <keniiarruda@gmaill.com>
2026-04-30 11:24:38 -0300

    fix: remove application.css asset and add missing CRUD actions to PlantsController

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -16,6 +16,10 @@ class PlantsController < ApplicationController
     end
   end
 
+  def new
+    @plant = Plant.new
+  end
+
   def create
     @plant = Plant.new(plant_params)
     if @plant.save
@@ -28,6 +32,34 @@ class PlantsController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @plant.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def edit
+    @plant = Plant.find(params[:id])
+  end
+
+  def update
+    @plant = Plant.find(params[:id])
+    if @plant.update(plant_params)
+      respond_to do |format|
+        format.html { redirect_to plant_path(@plant), notice: "Planta atualizada com sucesso!" }
+        format.json { render json: @plant }
+      end
+    else
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @plant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+    respond_to do |format|
+      format.html { redirect_to plants_path, notice: "Planta removida com sucesso!" }
+      format.json { head :no_content }
     end
   end
 
