e3c3a6cc3eaf6abb13ea60e5182afa589fe4e239
Kênia Guimaraes <keniiarruda@gmaill.com>
2026-06-02 15:36:43 -0300

    feat: implement basic authentication flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "dashboard#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 end
