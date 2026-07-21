948b56c47fbbabadbecb99ac4504a1b6ddb5393d
Kênia Guimaraes <keniiarruda@gmaill.com>
2026-07-21 12:44:38 -0300

    fix: query class_name via join on solid_queue_jobs
    class_name column does not exist on solid_queue_scheduled_executions
    table. Join through job association instead.

--- config/initializers/plant_reminder_schedule.rb
+++ config/initializers/plant_reminder_schedule.rb
@@ -3,7 +3,7 @@ Rails.application.config.after_initialize do
   next unless defined?(SolidQueue::ScheduledExecution)
   next unless defined?(PlantReminderJob)
 
-  if SolidQueue::ScheduledExecution.where(class_name: "PlantReminderJob").none?
+  if SolidQueue::ScheduledExecution.joins(:job).where(solid_queue_jobs: { class_name: "PlantReminderJob" }).none?
     Rails.logger.info("[migarden] Scheduling initial PlantReminderJob for next 08:00")
     PlantReminderJob.set(wait_until: PlantReminderJob.next_run_at).perform_later
   end
